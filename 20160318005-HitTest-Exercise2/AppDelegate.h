e5be7b7c1fab76c26ceceb41758aedb395156ee2
raineryue <it_jinlei.yue@163.com>
2016-03-18 22:38:13 +0800

    Initial Commit

---
+++ 20160318005-HitTest-Exercise2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160318005-HitTest-Exercise2
+//
+//  Created by Rainer on 16/3/18.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
